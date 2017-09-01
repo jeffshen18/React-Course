75da576131dfffd594fd3906a590e4900b3aa7b9
jeffreyshen18 <jeffreyshen@Jeffreys-MacBook-Pro-2.local>
2017-08-31 22:57:51 -0600

    building from scratch

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyDRibTMShfoTldafBAXr8nYpToCqaq-8mQ';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Game of Thrones'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render(){
+      return (
+       <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
