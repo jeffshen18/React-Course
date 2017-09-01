64e2aa3bb507571cbdb9649e1511188ed7c96e4e
jeffshen18 <jeffreyshen18@gmail.com>
2017-09-01 11:58:24 -0600

    added dynamic rendering for selecting videos

--- src/index.js
+++ src/index.js
@@ -10,10 +10,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term: 'Game of Thrones'}, videos => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'Game of Thrones'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
     });
   }
 
@@ -21,12 +27,13 @@ class App extends Component {
       return (
        <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect = {(selectedVideo) => this.setState({selectedVideo})} />
       </div>
     );
   }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-////
